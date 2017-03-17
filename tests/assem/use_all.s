c111f667a685437650a0e4406aaea11d1839274e
Steven Chen <sccchenster@gmail.com>
2017-03-16 21:23:36 -0700

    Update use all test

--- tests/assem/use_all.s
+++ tests/assem/use_all.s
@@ -3,7 +3,7 @@ lbl1:	addi $s0 $0 -1
 	lui  0x3FF
 	mflo $s2 
 	ori  $s2 $s2 0x3F
-	beq  $s0 $s2 lbl2
+	bne  $s0 $s2 lbl2
 	andi $s0 $s0 0
         and  $s1 $s1 $s2
      	xor  $s1 $s1 $s2
@@ -11,18 +11,18 @@ lbl1:	addi $s0 $0 -1
 lbl2:	slt  $a0 $s2 $s1
 	bne  $a0 $0  lbl3
 	slti $v0 $s2 0x1
-lbl3:	addi $v0 $v0 0x3F
+lbl3:	addi $v0 $v0 0x1F
         mult $v0 $v0
     	mfhi $s1 
 	mflo $s0
 	addi $sp $0 0x8
 	sh   $s0 0($sp)
-	lh   $s0 0($sp)
+	lh   $sp 0($sp)
 	j    $ra lbl4
 lbl4:	beq  $s0 $0 END
 	sll  $s0 $0 $0
 	srl  $s0 $0 $0
-	jr $0  $ra 0xF
+	jr $0  $ra 0x3E
 END:   	addi $s0 $0 1
          
 
